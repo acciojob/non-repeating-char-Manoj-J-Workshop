fbae23022c00eca396e6b62d14f8b3ef317ace90
Manoj-J-Workshop <manojj91298@gmail.com>
2025-08-30 17:07:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let charCount = {};
+
+	for(let ch of str){
+		charCount[ch] = 1 + (charCount[ch] || 0);
+	}
+
+	for(let char of str){
+		if(charCount[char] === 1){
+			return char;
+		}
+	}
+
+	return null;
+	
 }
+
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
